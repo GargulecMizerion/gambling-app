814cdfabd80c2025ab0b0b65e14f5c85975fa0fc
GargulecMizerion <xmaszekx@gmail.com>
2025-01-15 19:32:47 +0100

    Dodany historyItem wyswietlany w HistoryScreen

--- app/(screens)/HistoryScreen.js
+++ app/(screens)/HistoryScreen.js
@@ -1,11 +1,28 @@
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView, ActivityIndicator} from 'react-native'
 import React from 'react'
+import {tabNavHeader} from "@/app/components/tabNavHeader";
+import {homeItem} from "@/app/components/homeItem";
+import historyItem from "@/app/components/historyItem";
 
 const HistoryScreen = () => {
     return (
-        <View>
-            <Text>HistoryScreen</Text>
-        </View>
+        <>
+            <View className={"w-full p-5 bg-primary h-full"}>
+                {tabNavHeader()}
+                <ScrollView>
+                <View>
+                   {historyItem({
+                       amount: 20,
+                       odd: 3.20,
+                       status: 0,
+                       date: "12-12-2024"
+                   })}
+                </View>
+
+
+                </ScrollView>
+            </View>
+        </>
     )
 }
 export default HistoryScreen
---
+++ app/components/historyItem.js
@@ -0,0 +1,32 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export const HistoryItem = ({amount, odd, status, date}) => {
+
+    let color = status == -1 ? "border-red" : status == 0 ? "border-black" : "border-green";
+
+    return (
+        <View className={"w-full bg-secondaryGray border-4 rounded-xl p-[10px]" + color}>
+            <View  className={"flex-row justify-between"}>
+                <View>
+                    <Text className={"text-xl"}>{status == -1 ? "PRZEGRANY" : status == 0 ? "OTWARTY" : "WYGRANY"}</Text>
+                </View>
+                <View>
+                <Text  className={"text-xl"}>{date}</Text>
+                    </View>
+            </View>
+
+            <View className={"flex-row justify-between"}>
+                <View className={""}>
+                    <Text  className={"text-xl"}>KWOTA: ${amount}</Text>
+                    <Text  className={"text-xl"}>KURS: {odd}</Text>
+                </View>
+                <View className={"flex justify-center"}>
+                <Text  className={"text-4xl"}>${amount * odd}</Text>
+                </View>
+            </View>
+
+        </View>
+    )
+}
+export default HistoryItem
